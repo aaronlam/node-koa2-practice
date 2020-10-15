0774c63a5c607157c3585761b47863381eabd716
aaronlam <a@aaronlam.xyz>
2020-10-15 17:59:54 +0800

    generate koa project

---
+++ app.js
@@ -0,0 +1,46 @@
+// 依赖包加载
+const Koa = require('koa')
+const app = new Koa()
+const views = require('koa-views')
+const json = require('koa-json')
+const onerror = require('koa-onerror')
+const bodyparser = require('koa-bodyparser')
+const logger = require('koa-logger')
+
+const index = require('./routes/index')
+const users = require('./routes/users')
+
+// 错误处理
+onerror(app)
+
+// 中间件加载
+app.use(bodyparser({
+  enableTypes:['json', 'form', 'text']
+}))
+app.use(json())
+app.use(logger())
+app.use(require('koa-static')(__dirname + '/public'))
+
+// web界面渲染模板加载
+app.use(views(__dirname + '/views', {
+  extension: 'pug'
+}))
+
+// 自定义日志
+app.use(async (ctx, next) => {
+  const start = new Date()
+  await next()
+  const ms = new Date() - start
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+})
+
+// 自定义路由
+app.use(index.routes(), index.allowedMethods())
+app.use(users.routes(), users.allowedMethods())
+
+// error-handling
+app.on('error', (err, ctx) => {
+  console.error('server error', err, ctx)
+});
+
+module.exports = app
--- index.js
+++
@@ -1,8 +0,0 @@
-const koa = require("koa");
-const app = new koa();
-
-app.use(function* () {
-  this.body = "Hello World";
-});
-
-app.listen(3000);
